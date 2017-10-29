629d673c7f5c5557834ad9f9818bc510bd9ee117
c3zar <c3zar@protonmail.ch>
2017-10-29 18:21:34 +0000

    add super form install

--- src/app/layouts/full-layout.component.ts
+++ src/app/layouts/full-layout.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import { ToasterService} from 'angular2-toaster';
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/observable/fromEvent';
@@ -11,7 +11,7 @@ import 'rxjs/add/operator/mapTo';
     selector: 'app-dashboard',
     templateUrl: './full-layout.component.html'
 })
-export class FullLayoutComponent implements OnInit, OnDestroy {
+export class FullLayoutComponent {
     public subscriptions = { online: null, userData: null };
     public status: { isopen: boolean, online: boolean } = { isopen: false, online: true };
 
@@ -33,23 +33,5 @@ export class FullLayoutComponent implements OnInit, OnDestroy {
             // -->Set: status
             this.status.online = on;
           });
-    }
-
-    public toggled(open: boolean): void {
-        console.log('Dropdown is now: ', open);
-    }
-
-    public toggleDropdown($event: MouseEvent): void {
-        $event.preventDefault();
-        $event.stopPropagation();
-        this.status.isopen = !this.status.isopen;
-    }
-
-    ngOnInit() {
-        console.log('init main component');
-    }
-    ngOnDestroy(): void {
-      this.subscriptions.online.unsubscribe();
-      this.subscriptions.userData.unsubscribe();
     }
 }
