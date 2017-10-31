b7d0826073aaba3cf0074a73ecc43da301a08177
c3zar <c3zar@protonmail.ch>
2017-10-31 17:46:38 +0000

    new login page

--- src/app/pages/login.component.ts
+++ src/app/pages/login.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup} from '@angular/forms';
+import { SuperForm } from 'angular-super-validator';
+import { UserInterface } from '../interfaces/user.interface';
+import { HttpClientService } from '../providers/http/http-client.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: 'login.component.html'
 })
 export class LoginComponent {
+  public formGroup: FormGroup;
 
-  constructor() { }
+  get username(): FormControl { return this.formGroup.get('username') as FormControl; }
+  get password(): FormControl { return this.formGroup.get('password') as FormControl; }
 
+  constructor(
+    private http: HttpClientService,
+    private router: Router
+  ) {
+    this.formGroup = UserInterface.newLoginForm();
+  }
+
+  /**
+   * Login with current user/pass
+   */
+  public login(): void {
+
+    if (this.formGroup.valid) {
+      this.formGroup.disable();
+      // -->Set: data
+      const data = this.formGroup.getRawValue();
+            data.grant_type = 'password';
+
+      this.http.login(data)
+          .subscribe(ok => {
+
+            this.router.navigate(['/dashboard']);
+
+          }, err => {
+            console.error('error', err);
+            setTimeout(() => { this.formGroup.enable(); }, 1500);
+          });
+    } else {
+      const errors = SuperForm.getAllErrorsFlat(this.formGroup);
+      console.error(errors);
+    }
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { p404Component } from './404.component';
 import { p500Component } from './500.component';
@@ -6,14 +8,25 @@ import { LoginComponent } from './login.component';
 import { RegisterComponent } from './register.component';
 
 import { PagesRoutingModule } from './pages-routing.module';
+import { HttpClientService } from '../providers/http/http-client.service';
+import { UserService } from '../providers/user/user.service';
 
 @NgModule({
-  imports: [ PagesRoutingModule ],
-  declarations: [
-    p404Component,
-    p500Component,
-    LoginComponent,
-    RegisterComponent
-  ]
+    imports: [
+      CommonModule,
+      PagesRoutingModule,
+      FormsModule,
+      ReactiveFormsModule
+    ],
+    declarations: [
+        p404Component,
+        p500Component,
+        LoginComponent,
+        RegisterComponent
+    ],
+    providers: [
+        HttpClientService,
+        UserService
+    ]
 })
 export class PagesModule { }
